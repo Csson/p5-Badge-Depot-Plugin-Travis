3c22a768b2096d7eb35cb28ab78b74031f6ad933
Erik Carlsson <info@code301.com>
2015-02-17 13:38:10 +0100

    Init

---
+++ lib/Badge/Depot/Plugin/Travis.pm
@@ -0,0 +1,29 @@
+use 5.10.1;
+use strict;
+use warnings;
+
+package Badge::Depot::Plugin::Travis;
+
+# VERSION
+# ABSTRACT: Short intro
+
+
+
+1;
+
+
+__END__
+
+=pod
+
+=head1 SYNOPSIS
+
+    use Badge::Depot::Plugin::Travis;
+
+=head1 DESCRIPTION
+
+Badge::Depot::Plugin::Travis is ...
+
+=head1 SEE ALSO
+
+=cut
---
+++ t/01-basic.t
@@ -0,0 +1,9 @@
+use strict;
+use warnings;
+
+use Test::More;
+use if $ENV{'AUTHOR_TESTING'}, 'Test::Warnings';
+use Badge::Depot::Plugin::Travis;
+
+fail('this test is TODO!');
+done_testing;
